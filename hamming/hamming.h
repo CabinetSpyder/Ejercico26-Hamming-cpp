b07248ec8f2a4003c955cec45c3dae8ff788c1e3
AndresAAV <andres.artaloytia@gmail.com>
2025-02-26 15:21:34 +0100

    Primera iteracion de la solucion

--- hamming/hamming.h
+++ hamming/hamming.h
@@ -1,7 +1,12 @@
 #if !defined(HAMMING_H)
 #define HAMMING_H
 
+#include <string>
+#include <stdexcept> //Para std::domain_error
+
 namespace hamming {
+
+    int compute(std::string_view s1, std::string_view s2);
 
 }  // namespace hamming
 
